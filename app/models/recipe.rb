8dd4207839fc8221864a418efc33e9634c513c71
mthorry <mthorry@gmail.com>
2017-08-15 17:46:04 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,19 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
 end
+
+# :ingredients_attributes => {
+#       "0" => {
+#         :name => "water",
+#         :quantity => "1 gallon"
+#       },
+#       "1" => {
+#         :name => "corn",
+#         :quantity => "1 ear"
+#       },
+#       "2" => {
+#         :name => "flour",
+#         :quantity => "1 cup"
+#       }
+#     }
